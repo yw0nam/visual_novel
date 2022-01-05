6b2fb8ef1e66b00dbb27b06c8930a59d82c894f9
yw0nam <spow2544@gmail.com>
2022-01-05 14:53:22 +0900

    update code

--- TTS.eda.py
+++ TTS.eda.py
@@ -5,24 +5,62 @@ import numpy as np
 from glob import glob
 import os, subprocess
 from tqdm import tqdm
-data = pd.read_csv('/mnt/hdd/spow12/visual_novel/data.csv')
+data = pd.read_csv('/data1/spow12/datas/visual_novel/data.csv')
 
 # %%
 data_voice = data.dropna(subset=['voice'])
+data_voice = data_voice[data_voice['text'].map(lambda x: '[・]' not in x)]
+comp = re.compile("\u3000")
+data_voice['text'] = data_voice['text'].map(lambda x: re.sub(comp, '', x))
 # %%
-riddle_joker_root = '/mnt/hdd/spow12/visual_novel/RiddleJoker/voice/'
-riddle_joker_save = '/mnt/hdd/spow12/visual_novel/RiddleJoker/wav/'
-riddle_joker = data_voice[data_voice['game_name']=='RiddleJoker']
+data_to_normalize = data_voice[data_voice['text'].map(lambda x: ']' in x)]
 # %%
-riddle_joker_voices = glob(riddle_joker_root+'/*.ogg')
-# %%
-for path in tqdm(riddle_joker_voices):
-    command = ("ffmpeg -i {input_path} -b 768k -ac 1 -ar 48000 {output_path}.wav".format(input_path=path, 
-                                                                                     output_path=os.path.join(riddle_joker_save, os.path.basename(path).split('.')[0])))
-    output = subprocess.call(command, shell=True, stdout=None)
 
 # %%
-riddle_joker_voices = glob(riddle_joker_save+'/*')
 # %%
-len(riddle_joker_voices)
+comp = re.compile("[[][ぁ-ゔァ-ヴ\sー]*[]]*")
+pronun_dicts = []
+for i in tqdm(range(len(data_to_normalize))):
+    text = data_to_normalize['text'].iloc[i]
+    iter_obj = comp.finditer(text)
+    prev_end = None
+    words = ""
+    pronun = ""
+    for iter in iter_obj:
+        if prev_end == None:
+            words += text[iter.end()]
+            pronun += iter[0][1:-1]
+        elif iter.start() - prev_end <= 2:
+            words += text[iter.end()]
+            pronun += iter[0][1:-1]
+        elif iter.start() - prev_end >= 3:
+            pronun_dicts.append({
+                "words": words,
+                "pronun":pronun,
+                "game" : data_to_normalize['game_name'].iloc[i],
+                "index": i
+            })
+            words = ""
+            pronun = ""
+        prev_end = iter.end()
+    if words == "":
+        words += text[iter.end()]
+        pronun += iter[0][1:-1]
+    pronun_dicts.append({
+        "words": words,
+        "pronun":pronun,
+        "game" : data_to_normalize['game_name'].iloc[i],
+        "index": i
+    })
+# %%
+text = data_to_normalize['text'].iloc[i]
+t = pd.DataFrame(pronun_dicts)
+# %%
+t.to_excel('./temp.xlsx', index=False)
+
+
+# %%
+data_voice[data_voice['text'].map(lambda x: '頭文字' in x)]
+# %%
+data_to_normalize.iloc[226]['text']
 # %%
