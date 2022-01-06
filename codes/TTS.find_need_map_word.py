2034c9e0176082e8aaed1c2399f9057c13f67b57
yw0nam <spow2544@gmail.com>
2022-01-06 17:04:26 +0900

    update mapping code

--- codes/TTS.find_need_map_word.py
+++ codes/TTS.find_need_map_word.py
@@ -54,20 +54,46 @@ text = data_to_normalize['text'].iloc[i]
 t = pd.DataFrame(pronun_dicts)
 # %%
 df = pd.read_excel('./../data/temp_2.xlsx')
-
 df_non_chara = df.query("need_map == 'Y' and etc != 'Chara_name'")
 # %%
 df_non_chara = df_non_chara.drop_duplicates(subset='words')
 # %%
 df_non_chara = df_non_chara.drop(89)
 # %%
+def remove_word_and_replace_pronun(text):
+    expr = re.compile("[[][ぁ-ゔァ-ヴ\sー]*[]]*")
+    text_to_remove = []
+    iter_obj = expr.finditer(text)
+    
+    for obj in iter_obj:
+        text_to_remove.append(text[obj.end()])
+        
+    expr = '|'.join(text_to_remove+["[[]", "[]]"])
+    regx_remove_text = re.compile(expr)
+    return re.sub(regx_remove_text, "",  text)
+
 # %%
 
-data_voice['normalized_text'] = data_voice['text'].map(lambda x: re.sub(comp, "", x))
+data_voice['normalized_text'] = data_voice['text'].map(lambda x: remove_word_and_replace_pronun(x))
 # %%
-expr = re.compile("[[][ぁ-ゔァ-ヴ\sー]*[]]*")
-def remove_word_and_replace_pronun(text, expr=expr):
-    iter_obj = expr.finditer(text)
-    for obj in iter_obj:
-        
-# for i in tqdm(range(len(data_to_normalize))):
+game_ls = ['SenrenBanka', 'RiddleJoker', 'CafeStella']
+# %%
+game_df = df_non_chara.query("game == @game_ls[0]")
+# %%
+text = data_voice[data_voice['text'].map(lambda x: '叢雨丸' in x)].iloc[0]['text']
+# %%
+expr = '|'.join(game_df['words'].to_list())
+comp = re.compile(expr)
+iter_obj = comp.finditer(text)
+# %%
+for obj in iter_obj:
+    print(obj[0])
+    
+# %%
+game_df.query("words == @obj[0]")['pronun'][0]
+# %%
+game_df['words'].to_list()
+# %%
+dict(zip(game_df.words, game_df.pronun))
+
+# %%
