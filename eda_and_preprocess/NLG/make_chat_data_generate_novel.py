ef14d8689bf1931deeea7e2201ad8d3753af991b
YoungWoo nam <spow2544@gmail.com>
2024-07-18 16:50:07 +0900

    Feature: Add novel generating and fill mask instruction

--- eda_and_preprocess/NLG/make_chat_data_generate_novel.py
+++ eda_and_preprocess/NLG/make_chat_data_generate_novel.py
@@ -11,10 +11,11 @@ from  matplotlib import pyplot as plt
 from utils import jdump, jload
 import numpy as np
 # %%
-system_dict = jload('./data/system_dict_updated.json')
+chara_bg_dicts = jload('./data/system_dict_updated.json')
 system_message = """This is an RP (roleplay) chat. Our characters could come from all sorts of places--like movies, video games, books, or even anime. Make sure that you follow all my instructions and setup, based on the details to follow
-I'm going to give you a character's name, a background, and a {game_name}.
+I'm going to give you an character name, a background, and a {game_name}.
 I want you to respond and answer like characters using the tone, manner and vocabulary characters would use. 
+Here is Main Character's backgrounds.
 """
 # %%
 data = pd.read_csv('~/Desktop/data/visual_novel/yuzusoft/data.csv')
@@ -33,8 +34,8 @@ name_ls = temp[temp > 1500].index.to_list()
 data['name'] = data['name'].replace({'昂晴': 'ユーザー', '暁': 'ユーザー', '将臣': 'ユーザー'})
 data['name'] = data['name'].fillna('ユーザー')
 # %%
-min_context_window = 8
-max_context_window = 15
+min_context_window = 10
+max_context_window = 20
 out_ls = []
 index = 0
 # %%
@@ -46,7 +47,7 @@ def apply_fn(name, text_remove_yomigana, dialog_type):
     if dialog_type == 'monologue':
         text += text_remove_yomigana
     else:
-        text += f"{name}:" + text_remove_yomigana + '\n'
+        text += f"{name}:" + text_remove_yomigana
     return text
 for name, group in tqdm(grouped):
     idx = 0
@@ -61,23 +62,60 @@ for name, group in tqdm(grouped):
         })
         idx += context_size
 # %%
-out[0]
+def make_masked_chat_prediction(mapped_text, characters, game_name, chara_bg_dicts, mask_portion=0.4):
+    while True:
+        flag = 0
+        masked_ls =[mapped_text[0]]
+        out_mapped_text = []
+        mask_ = np.random.choice([0, 1], size=len(mapped_text), p=[1-mask_portion, mask_portion])
+        for text, mask, chara in zip(mapped_text, mask_, characters):
+            if mask and (chara in chara_bg_dicts): #Masking only main characters text
+                if ':' in text:
+                    index = text.find(':')
+                    masked_ls.append(text[:index+1] +' [MASK]')
+                else:
+                    masked_ls.append('[MASK]')
+                out_mapped_text.append(text)
+                flag = 1
+            else:
+                masked_ls.append(text)
+        if flag:
+            break
+
+    output = "\n".join([f"{i+1}: {out}" for i, out in enumerate(out_mapped_text)])
+    chara_bgs = []
+    for chara in characters:
+        if chara in chara_bg_dicts:
+            chara_bgs.append(chara_bg_dicts[chara])
+    instruction = system_message.format(game_name=game_name) +"\n".join(chara_bgs) +"\n与えられた台詞を見て、[MASK]で省略されたキャラクターの台詞を上から順番に生成してください。"
+    inputs = "\n".join(masked_ls)
+    return instruction, inputs, output, 'fill_mask'
+
+def make_novel_generate(mapped_text, characters, game_name, chara_bg_dicts, init_portion=0.3):
+    
+    init_index = int(len(mapped_text) * init_portion)
+    init_mapped_text = mapped_text[:init_index]
+    target_mapped_text = mapped_text[init_index:]
+    chara_bgs = []
+    for chara in characters:
+        if chara in chara_bg_dicts:
+            chara_bgs.append(chara_bg_dicts[chara])
+    instruction = system_message.format(game_name=game_name) +"\n".join(chara_bgs) +"\n与えられたキャラクターを利用して、キャラクターの会話につながる会話を生成してください。"
+    inputs = "\n".join(init_mapped_text)
+    output = "\n".join(target_mapped_text)
+    return instruction, inputs, output, 'novel_generate'
 # %%
 df = pd.DataFrame(out)
 # %%
-masked_ls = []
-texts = out[0]['mapped_text']
-mask_ = np.random.choice([0, 1], size=len(texts), p=[0.75, 0.25])
+tqdm.pandas()
+instruction, inputs, output, source = zip(*df.progress_apply((lambda x: 
+    make_masked_chat_prediction(**x, chara_bg_dicts=chara_bg_dicts) 
+    if random.randint(0,1) == 0  and any([character in chara_bg_dicts for character in list(set(x['characters']))]) else 
+    make_novel_generate(**x, chara_bg_dicts=chara_bg_dicts) 
+),axis=1))
+    
 # %%
-for text, mask in zip(texts, mask_):
-    if mask:
-        if ':' in text:
-            index = text.find(':')
-            masked_ls.append(text[:index+1] +'[MASK]')
-        else:
-            masked_ls.append('[MASK]')
-    else:
-        masked_ls.append(text)        
+temp = pd.DataFrame({'insturuction': instruction, 'input': inputs, 'output': output, 'source': source})
 # %%
-masked_ls
+temp['source'].value_counts()
 # %%
